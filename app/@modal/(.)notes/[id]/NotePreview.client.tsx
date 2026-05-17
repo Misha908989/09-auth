3fcf7a70726bb426ef1d4d774fd7b74e52cee6ad
Misha908989 <mishazaharevich71@gmail.com>
2026-05-17 11:34:53 +0300

    try

---
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { fetchNoteById } from "@/lib/api";
+import css from "./interceptedNote.module.css";
+
+interface NotePreviewClientProps {
+  id: string;
+}
+
+export default function NotePreviewClient({ id }: NotePreviewClientProps) {
+  const { data: note, isLoading, isError } = useQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+    enabled: !!id,
+    refetchOnMount: false,
+  });
+
+  if (isLoading) return <p>Loading, please wait...</p>;
+  if (isError || !note) return <p>Something went wrong.</p>;
+
+  return (
+    <div className={css.item}>
+      <div className={css.header}>
+        <h2>{note.title}</h2>
+      </div>
+      <p className={css.tag}>{note.tag}</p>
+      <p className={css.content}>{note.content}</p>
+      <p className={css.date}>
+        {new Date(note.createdAt).toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+        })}
+      </p>
+    </div>
+  );
+}
